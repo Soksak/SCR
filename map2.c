401ea556b2428ea5b2e894ad7c3520a19fee95ac
Soksak <61745687+Soksak@users.noreply.github.com>
2020-12-10 17:41:54 +0100

    Add files via upload

---
+++ map2.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <sys/mman.h>
+
+int main()
+{
+
+    int fd = 0, fd_in;
+    char name[100];
+    struct stat stat_file;
+    char *addr;
+
+    while(1) {
+        fd = open("tekst1.txt", O_RDWR);
+        printf("Podaj nazwę pliku :\t");
+        scanf("%s", name);
+        
+
+        while(fd_in < 0) {
+            printf("Nie otwarto pliku\n");
+            printf("Podaj nową nazwę pliku:\t");
+            scanf("%s", name);
+        }
+
+        stat(name, &stat_file);
+        ftruncate(fd, stat_file.st_size);
+        addr = mmap(NULL, stat_file.st_size, PROT_READ|PROT_WRITE,MAP_SHARED, fd, 0);
+	fd_in = open(name, O_RDWR);
+        read(fd_in, addr, stat_file.st_size);
+	
+        close(fd);
+        close(fd_in);
+    }
+
+    munmap(addr, stat_file.st_size);
+    return 0;
+}
